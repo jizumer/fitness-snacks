a8c309f6dfc420c7af7cf194013cb54258903e0b
Juan I. Zumer <jizumer@gmail.com>
2022-12-24 01:24:52 +0100

    Initial project setup

---
+++ src/main/kotlin/com/jizumer/fitnesssnacks/FitnessSnacksApplication.kt
@@ -0,0 +1,11 @@
+package com.jizumer.fitnesssnacks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitnessSnacksApplication
+
+fun main(args: Array<String>) {
+    runApplication<FitnessSnacksApplication>(*args)
+}
