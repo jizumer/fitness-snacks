577df35eb104ff3d04043878b76536a7fbb71592
Juan I. Zumer <jizumer@gmail.com>
2022-12-25 21:31:51 +0100

    Assert that it returns json media type

--- src/test/kotlin/com/jizumer/fitnesssnacks/SnacksControllerTest.kt
+++ src/test/kotlin/com/jizumer/fitnesssnacks/SnacksControllerTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -19,8 +20,10 @@ internal class SnacksControllerTest @Autowired constructor(
     fun `should return the full list of snacks`() {
 
         mockMvc.get(baseUrl)
+            .andDo { print() }
             .andExpect {
                 status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
                 jsonPath("$[0].type") {
                     value(
                         "squats"
