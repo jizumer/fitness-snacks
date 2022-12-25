a9010241637b28a07ddb6c10ab1bb2852886094f
Juan I. Zumer <jizumer@gmail.com>
2022-12-25 22:17:24 +0100

    Extract controller to its own file

--- src/main/kotlin/com/jizumer/fitnesssnacks/FitnessSnacksApplication.kt
+++ src/main/kotlin/com/jizumer/fitnesssnacks/FitnessSnacksApplication.kt
@@ -2,9 +2,6 @@ package com.jizumer.fitnesssnacks
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FitnessSnacksApplication
@@ -13,9 +10,3 @@ fun main(args: Array<String>) {
     runApplication<FitnessSnacksApplication>(*args)
 }
 
-@RestController
-@RequestMapping("snacks")
-class SnacksController {
-    @GetMapping("")
-    fun getAll() = listOf(Snack("squats", null))
-}
---
+++ src/main/kotlin/com/jizumer/fitnesssnacks/SnacksController.kt
@@ -0,0 +1,12 @@
+package com.jizumer.fitnesssnacks
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("snacks")
+class SnacksController {
+    @GetMapping("")
+    fun getAll() = listOf(Snack("squats", null))
+}
