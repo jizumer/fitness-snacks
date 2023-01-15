af2679dd930c3a310e601c2145b0e7193e4f9f1c
Juan I. Zumer <jizumer@gmail.com>
2023-01-15 21:18:38 +0100

    Base case for SnacksService when its called for the first time

--- src/main/kotlin/com/jizumer/fitnesssnacks/SnacksService.kt
+++ src/main/kotlin/com/jizumer/fitnesssnacks/SnacksService.kt
@@ -5,4 +5,10 @@ import org.springframework.stereotype.Service
 @Service
 class SnacksService(val snacksRepository: SnacksRepository) {
     fun findAll(): List<Snack> = snacksRepository.findAll().toList()
+    fun next(): Snack? {
+        if (findAll().size.equals(0)) {
+            return null
+        }
+        return null
+    }
 }
--- src/test/kotlin/com/jizumer/fitnesssnacks/SnacksServiceTest.kt
+++ src/test/kotlin/com/jizumer/fitnesssnacks/SnacksServiceTest.kt
@@ -3,9 +3,9 @@ package com.jizumer.fitnesssnacks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import java.time.LocalDateTime
 
 internal class SnacksServiceTest {
@@ -20,8 +20,20 @@ internal class SnacksServiceTest {
         assertEquals(
             snacksService.findAll(), snacksMock
         )
-
-
         verify(atLeast = 1) { snacksRepository.findAll() }
+    }
+
+    @Test
+    fun `should return a new snack proposal when 'next' method is called for the first time`() {
+        val snacksRepository = mockk<SnacksRepository>()
+        val snacksService = SnacksService(snacksRepository)
+
+        val snacksMock = emptyList<Snack>()
+        every { snacksRepository.findAll() } returns snacksMock
+
+
+        assertNull(snacksService.next())
+        verify(exactly = 1) { snacksRepository.findAll() }
+        verify(exactly = 0) { snacksRepository.save(any<Snack>()) }
     }
 }
